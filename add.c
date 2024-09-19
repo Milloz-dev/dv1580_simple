8b0f256202fa7ab449575fb3c328e0ac70e73264
root <root@DESKTOP-9455VIS>
2024-09-19 22:31:42 +0200

    fixade add så det ger error om inte nummer

--- add.c
+++ add.c
@@ -5,10 +5,19 @@
 // add.c; Implement a solution that takes the first two arguments 
 //(argv[1], argv[2]) and adds them together. At this point, assume the arguments are positive INTEGERS only. 
 int main(int argc, char *argv[]) {
-    // Convert arguments to integers
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+     char *endptr;
 
+     // Convert the arguments to double
+    double num1 = strtod(argv[1], &endptr);
+    if (*endptr != '\0') {
+        fprintf(stderr, "Error: '%s' is not a valid number.\n", argv[1]);
+        return 1;
+    }
+    double num2 = strtod(argv[2], &endptr);
+    if (*endptr != '\0') {
+        fprintf(stderr, "Error: '%s' is not a valid number.\n", argv[2]);
+        return 1;
+    }
     // Calculate the sum
     int sum = num1 + num2;
 
